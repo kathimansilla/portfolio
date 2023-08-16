d86b368243e164f760ebf5b0a272ffcb2a2109e6
kathimansilla <mansillakatherina@gmail.com>
2023-08-16 15:35:23 +0200

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-//import reportWebVitals from './reportWebVitals';
-//de momento no se usar webvitals pero se podria usar para verificar que la aplicacion funciona correctamente
-
-
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
-  //me cargo esto porque se refersca la pagina mas veces de la cuenta aunque nos ayuda a programar mejor
-    <App />
-  //</React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
