be656e5aa7d220b4c18720e989ef8cfd02d50642
kathimansilla <mansillakatherina@gmail.com>
2023-08-23 20:47:38 +0200

    add link to resume and e-mail

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,12 +20,14 @@ const Header = () => {
         </form>
       <ul className='ul'>
           <li>
-            <button className='ul__cvBtn'>
-              Resume
-            </button>
+            <a href="https://drive.google.com/file/d/1Ne4zV7TIRoakofjxzIH7jBf7foff8L3u/view?usp=sharing" target='_blank' rel='noreferrer'>
+              <button className='ul__cvBtn'>
+                Resume
+              </button>
+            </a>
           </li>
           <li>
-            <button className='ul__contactBtn'>Contact</button>
+            <a href={'mailto:mansillakatherina@gmail.com'}><button className='ul__contactBtn'>Contact</button></a>
           </li>
           <li className='ul__linkedin'>
             <a className='ul__linkedin__link' href="https://www.linkedin.com/in/katherina-mansilla/" target='_blank' rel='noreferrer'><img className='linkedinLogo' src={linkedin} alt="linkedin logo" /></a>
