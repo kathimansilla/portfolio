27d0fe32d17b3f0bea32b8312dcbc7496d3fbe32
kathimansilla <mansillakatherina@gmail.com>
2023-08-28 13:15:10 +0200

    add .gitignore

---
+++ backup/src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer__text" data-copied-text="¡Texto para copiar!">mansillakatherina@gmail.com</p>
+        <span>2023</span>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
